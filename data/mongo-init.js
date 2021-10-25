cc04b071009c5e7fb29e039982e39a3367d342c8
Sergi Jimenez <tripledes@users.noreply.github.com>
2021-10-25 21:55:16 +0200

    feat: docker-compose
    * Build app container
    * Run app stack from docker/podman-compose
    * Add sample data for the DB

---
+++ data/mongo-init.js
@@ -0,0 +1,7 @@
+
+db.quotes.drop();
+db.quotes.insert({ tags: ['life', 'science', 'wisdom'], author: 'Isaac Asimov', text: 'The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.' });
+db.quotes.insert({ tags: ['humor'], author: 'Isaac Asimov', text: 'Those people who think they know everything are a great annoyance to those of us who do.'});
+db.quotes.insert({ tags: ['lies', 'lying', 'memory', 'truth'], author: 'Mark Twain', text: 'If you tell the truth, you do not have to remember anything.' });
+db.quotes.insert({ tags: ['friend', 'friendship', 'knowledge', 'love'], author: ' Elbert Hubbard', text: 'A friend is someone who knows all about you and still loves you.' });
+quit();
